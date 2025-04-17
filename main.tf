5a0a3dd7a946f4212c01cdac1ba1ced7ac7fbeab
devmani2025 <devopsmani2025@gmail.com>
2025-04-17 21:42:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-080b1a55a0ad28c02"
+ami = "ami-07a6f770277670015"
 instance_type = "t2.medium"
 key_name = "netflix"
-vpc_security_group_ids = ["sg-0b5d76c8dd04202fc"]
+vpc_security_group_ids = ["sg-0e852a35e681ac8af"]
 tags = {
 Name = var.instance_names[count.index]
 }
