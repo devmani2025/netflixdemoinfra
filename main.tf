2ddf37a8041388a11a51c409b2363caf5dbd7aac
devmani2025 <devopsmani2025@gmail.com>
2025-04-17 22:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
 region = "ap-south-1"
 }
-
 resource "aws_instance" "one" {
 count = 4
 ami = "ami-080b1a55a0ad28c02"
@@ -12,7 +11,6 @@ tags = {
 Name = var.instance_names[count.index]
 }
 }
-
 variable "instance_names" {
 default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
