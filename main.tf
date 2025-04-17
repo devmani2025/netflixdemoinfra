8b56eba794baf139af6c6faed241cb508f0fe44a
devmani2025 <devopsmani2025@gmail.com>
2025-04-17 15:16:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0dc1b999cc147d528 "
+ami = "ami-0dc1b999cc147d528"
 instance_type = "t2.medium"
 key_name = "netflix"
 vpc_security_group_ids = ["sg-0b5d76c8dd04202fc"]
